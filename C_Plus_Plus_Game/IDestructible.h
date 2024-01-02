0dc5f60869dc26a76c9fd948fcf901775cb4e988
BabisDros <150071850+BabisDros@users.noreply.github.com>
2024-01-02 14:41:41 +0200

    Minor changes του IDestructible

--- C_Plus_Plus_Game/IDestructible.h
+++ C_Plus_Plus_Game/IDestructible.h
@@ -1,16 +1,17 @@
 #pragma once
-//INTERFACE. Objects that can be destructed should inherit this
+//INTERFACE. Objects that can be destructed should implement this
 class IDestructible 
 {
 public:
     IDestructible(int initialHealth):m_initialHealth(initialHealth), m_currentHealth(initialHealth) {};
     IDestructible() {};
     virtual ~IDestructible() {}
-    virtual int getHealth() const = 0;
-    virtual void resetHealth() = 0;
-    virtual void takeDamage(const int& damage) = 0;
-    virtual bool isAlive() const = 0;
+    virtual int getHealth() const { return m_currentHealth; };
+    virtual void resetHealth() { m_currentHealth = m_initialHealth; };
+    virtual void takeDamage(const int& damage) { m_currentHealth -= damage; };
+    virtual bool isAlive() const { return m_currentHealth > 0; };
     virtual void destroy() = 0;
+    virtual void instantiateParticles() = 0;
 
 protected:
     int m_initialHealth = 0;
